0e42b84e4c1b5ce7efb6eb0a0a154ec3ce0ae3cf
Felix Bräunling <felix.braeunling@methodpark.de>
2021-12-01 06:11:37 +0100

    Add solutions for day 1

---
+++ src/day1/mod.rs
@@ -0,0 +1,73 @@
+use aoc_downloader::download_day;
+
+const DAY: u32 = 1;
+
+fn get_input() -> Vec<String> {
+    use std::io::BufRead;
+    download_day((DAY) as u32, "input").unwrap();
+
+    let file = std::fs::File::open(format!("input/input{}.txt", DAY)).unwrap();
+    let reader = std::io::BufReader::new(&file);
+    reader.lines().collect::<Result<_, _>>().unwrap()
+}
+
+fn parse_input(input: Vec<String>) -> Vec<u32> {
+    input
+        .iter()
+        .map(|v| v.parse::<u32>().unwrap())
+        .collect::<Vec<_>>()
+}
+
+pub fn run_day() {
+    let input = get_input();
+    let input = parse_input(input);
+    println!("Running day {}:\n\tPart1 {}\n\tPart2 {}", DAY, part1(&input), part2(&input));
+}
+
+fn part1(input: &Vec<u32>) -> u32 {
+    let mut increased = 0;
+    let mut prev_val = input[0];
+    for val in input {
+        if *val > prev_val{
+            increased += 1;
+        }
+        prev_val = *val;
+    }
+    increased
+}
+
+fn part2(input: &Vec<u32>) -> u32 {
+    let mut increased = 0;
+    let mut prev_val = input[0] + input[1] + input[2];
+    for val in input.windows(3) {
+        let sum = val.iter().sum();
+        if sum > prev_val{
+            increased += 1;
+        }
+        prev_val = sum;
+    }
+    increased
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day0_part1_output() {
+        let input = parse_input(get_input());
+        assert_eq!(1215, part1(&input));
+    }
+
+    #[test]
+    fn day0_part1_testcase1() {
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260,263];
+        assert_eq!(7, part1(&input));
+    }
+
+    #[test]
+    fn day0_part2_output() {
+        let input = parse_input(get_input());
+        assert_eq!(1150, part2(&input));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod day_template;
+mod day1;
 
 pub fn get_days() -> Vec<fn()> {
     vec![
         day_template::run_day,
+        day1::run_day,
     ]
 }
