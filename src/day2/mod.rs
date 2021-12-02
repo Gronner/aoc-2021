e2391a9bebaf0799756ae08cbc696f2372ded61f
Gronner <felix.braeunling@t-online.de>
2021-12-02 22:11:19 +0100

    Cleanup solution 2 some more

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -3,6 +3,68 @@ use regex::Regex;
 
 const DAY: u32 = 2;
 
+enum Operation {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
+}
+
+impl From<&String> for Operation {
+    fn from(input: &String) -> Self {
+        let re = Regex::new(r"(.+) (\d)").unwrap();
+        re.captures(input).and_then(|captured| {
+            let value = captured[2].parse::<i32>().unwrap();
+            match &captured[1] {
+                "forward" => Some(Operation::Forward(value)),
+                "down" => Some(Operation::Down(value)),
+                "up" => Some(Operation::Up(value)),
+                _ => None,
+            }
+        }).unwrap()
+    }
+}
+
+struct Submarine {
+    position: i32,
+    depth: i32,
+    aim: i32,
+}
+
+impl Submarine {
+    pub fn dive(&mut self, operation: &Operation) {
+        match operation {
+            Operation::Forward(movement) => self.position += movement,
+            Operation::Down(movement) => self.depth += movement,
+            Operation::Up(movement) => self.depth -= movement,
+        };
+    }
+
+    pub fn dive2(&mut self, operation: &Operation) {
+        match operation {
+            Operation::Forward(movement) => {
+                self.position += movement;
+                self.depth += self.aim * movement;
+            },
+            Operation::Down(change) => self.aim += change,
+            Operation::Up(change) => self.aim -= change,
+        };
+    }
+
+    pub fn get_traveled_distance(self) -> i32 {
+        self.position * self.depth
+    }
+}
+
+impl Default for Submarine {
+    fn default() -> Self {
+        Submarine {
+            position: 0,
+            depth: 0,
+            aim: 0,
+        }
+    }
+}
+
 fn get_input() -> Vec<String> {
     use std::io::BufRead;
     download_day((DAY) as u32, "input").unwrap();
@@ -12,12 +74,9 @@ fn get_input() -> Vec<String> {
     reader.lines().collect::<Result<_, _>>().unwrap()
 }
 
-fn parse_input(input: Vec<String>) -> Vec<Vec<String>> {
-    let re = Regex::new(r" ").unwrap();
+fn parse_input(input: Vec<String>) -> Vec<Operation> {
     input.iter()
-        .map(|i| re.split(i)
-            .map(|f| f.to_owned())
-            .collect())
+        .map(|inp| Operation::from(inp))
         .collect::<Vec<_>>()
 }
 
@@ -27,36 +86,20 @@ pub fn run_day() {
     println!("Running day {}:\n\tPart1 {}\n\tPart2 {}", DAY, part1(&input), part2(&input));
 }
 
-fn part1(input: &Vec<Vec<String>>) -> i32{
-    let mut pos_x: i32 = 0;
-    let mut pos_y: i32 = 0;
-    for inp in input {
-        match (inp[0].as_ref(), inp[1].parse::<i32>().unwrap()) {
-            ("forward", movement) => pos_x += movement,
-            ("down", movement) => pos_y += movement,
-            ("up", movement) => pos_y -= movement,
-            (_, _) => panic!(),
-        }
+fn part1(input: &Vec<Operation>) -> i32{
+    let mut uboot = Submarine::default();
+    for operation in input {
+        uboot.dive(operation);
     }
-    pos_x * pos_y
+    uboot.get_traveled_distance()
 }
 
-fn part2(input: &Vec<Vec<String>>) -> i32 {
-    let mut aim = 0;
-    let mut pos_x: i32 = 0;
-    let mut pos_y: i32 = 0;
-    for command in input {
-        match (command[0].as_ref(), command[1].parse::<i32>().unwrap()) {
-            ("forward", movement) => {
-                pos_x += movement;
-                pos_y += aim * movement;
-            }
-            ("down", change) => aim += change,
-            ("up", change) => aim -= change,
-            (_, _) => panic!(),
-        }
+fn part2(input: &Vec<Operation>) -> i32 {
+    let mut uboot = Submarine::default();
+    for operation in input {
+        uboot.dive2(operation);
     }
-    pos_x * pos_y
+    uboot.get_traveled_distance()
 }
 
 #[cfg(test)]
