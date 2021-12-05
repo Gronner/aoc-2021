567bfa2c03642470e49de49ffe33c6477410fd01
Gronner <felix.braeunling@t-online.de>
2021-12-05 21:18:12 +0100

    Make day 5 use Vector instead of tuple

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -3,26 +3,26 @@ use aoc_downloader::download_day;
 use num::signum;
 use regex::Regex;
 
-use crate::utils::table::Table;
+use crate::utils::{table::Table, coordinates::Vector};
 
 const DAY: u32 = 5;
 type InputType = Lines;
 
 #[derive(Debug)]
 struct Lines {
-    start: (usize, usize),
-    end: (usize, usize),
+    start: Vector<usize>,
+    end: Vector<usize>,
 }
 
 impl FromStr for Lines {
     type Err = std::num::ParseIntError;
     
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let re = Regex::new(r"(\d+),(\d+) -> (\d+),(\d+)").unwrap();
+        let re: Regex = Regex::new(r"(\d+),(\d+) -> (\d+),(\d+)").unwrap();
         Ok(re.captures(input).and_then(|captured| {
             Some(Lines {
-                start: (captured[1].parse::<usize>().unwrap(), captured[2].parse::<usize>().unwrap()),
-                end: (captured[3].parse::<usize>().unwrap(), captured[4].parse::<usize>().unwrap()),
+                start: Vector::from((captured[1].parse::<usize>().unwrap(), captured[2].parse::<usize>().unwrap())),
+                end: Vector::from((captured[3].parse::<usize>().unwrap(), captured[4].parse::<usize>().unwrap())),
             })
         }).unwrap())
     }
@@ -49,46 +49,36 @@ pub fn run_day() {
 fn part1(input: &Vec<InputType>) -> u32{
     let mut seafloor = Table::from_vecvec(vec![vec![0; 1000]; 1000]);
     for line in input {
-        let mut current_pos = line.start;
-        let vector = compute_normal_vector(line.start, line.end);
-        if !include_diagonals(vector) {
+        let vector = (&line.start - &line.end).normalized();
+        let mut current_pos = line.start.clone();
+        if !include_diagonals(&vector) {
             continue;
         }
-        seafloor[current_pos] += 1;
-        while line.end != current_pos {
-            current_pos = next_position(current_pos, vector);
-            seafloor[current_pos] += 1;
+        seafloor[&current_pos] += 1;
+        while &line.end != &current_pos {
+            current_pos = &current_pos + &vector;
+            seafloor[&current_pos] += 1;
         }
     }
     seafloor.get_vector().iter().filter(|&n| 1 < *n).count() as u32
 }
 
-fn include_diagonals(vector: (isize, isize)) -> bool {
-    !((vector.0 != 0) && (vector.1 != 0))
+fn include_diagonals(vector: &Vector<isize>) -> bool {
+    !((vector['x'] != 0) && (vector['y'] != 0))
 }
 
 fn part2(input: &Vec<InputType>) -> u32 {
     let mut seafloor = Table::from_vecvec(vec![vec![0; 1000]; 1000]);
     for line in input {
-        let mut current_pos = line.start;
-        let vector = compute_normal_vector(line.start, line.end);
-        seafloor[current_pos] += 1;
-        while line.end != current_pos {
-            current_pos = next_position(current_pos, vector);
-            seafloor[current_pos] += 1;
+        let mut current_pos = line.start.clone();
+        let vector = (&line.start - &line.end).normalized();
+        seafloor[&current_pos] += 1;
+        while &line.end != &current_pos {
+            current_pos = &current_pos + &vector;
+            seafloor[&current_pos] += 1;
         }
     }
     seafloor.get_vector().iter().filter(|&n| 1 < *n).count() as u32
-}
-
-fn compute_normal_vector(start: (usize, usize), end: (usize, usize)) -> (isize, isize) {
-    (num::signum(end.0 as isize - start.0 as isize),
-    num::signum(end.1 as isize - start.1 as isize))
-}
-
-fn next_position(position: (usize, usize), vector: (isize, isize)) -> (usize, usize) {
-    ((position.0 as isize + vector.0) as usize,
-    (position.1 as isize + vector.1) as usize)
 }
 
 #[cfg(test)]
