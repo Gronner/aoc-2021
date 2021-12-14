adb8ed29c77860cf150eaeb9598f7c56e4c389c3
Gronner <felix.braeunling@t-online.de>
2021-12-14 23:45:16 +0100

    Add preparation for day 15

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
 mod utils;
 
 pub fn get_days() -> Vec<fn()> {
@@ -35,5 +36,6 @@ pub fn get_days() -> Vec<fn()> {
         day12::run_day,
         day13::run_day,
         day14::run_day,
+        day15::run_day,
     ]
 }
